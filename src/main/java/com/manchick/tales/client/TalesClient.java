b2d4c7cc42c68ece48e442d8a63d0400b4a5de60
Manchick0 <endercreep109@gmail.com>
2024-08-04 09:57:12 +0200

    Initial Commit

---
+++ src/main/java/com/manchick/tales/client/TalesClient.java
@@ -0,0 +1,10 @@
+package com.manchick.tales.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TalesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
